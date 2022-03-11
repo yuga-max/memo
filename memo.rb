50899f897cf90e8b208ec7f968f9e50d4d0c0eb7
EC2 Default User <ec2-user@ip-172-31-45-194.ec2.internal>
2022-03-11 11:45:25 +0000

    メモアプリ

--- memo.rb
+++ memo.rb
@@ -1,26 +1,27 @@
-   require "csv"
-   puts "1(新規でメモを作成) 2(既存のメモ編集する)"
-   memo_type = gets.chomp
+    require "csv"
+    puts "1(新規でメモを作成) 2(既存のメモ編集する)"
+    memo_type = gets.chomp
    
     if memo_type == "1"
-    puts "拡張子を除いたファイルを入力してください。"
-    memo_type = gets.chomp
-    puts "メモしたい内容を記入してください"
-    puts "完成したらCtrl + Dを押します"
-    memo_type = STDIN.read
-    CSV.open("test.csv","w") do | csv |
-      csv << ["#{memo_type}"]
+     puts "拡張子を除いたファイルを入力してください。"
+     memo_type2 = gets.chomp
+     puts "メモしたい内容を記入してください"
+     puts "完成したらCtrl + Dを押します"
+     memo_type = STDIN.read
+     CSV.open("#{memo_type2}.csv","w") do | csv |
+     csv << ["#{memo_type}"]
     end 
     
     elsif memo_type == "2"
-    name = "test.csv"
-    puts "既存のcsv#{name}に追記します"
-    puts "編集したい内容を記入してください"
-    puts "完成したらCtrl + Dを押します"
-    memo_type = STDIN.read
-    CSV.open("test.csv","a") do | csv |
-      csv << ["#{memo_type}"]
+     puts "既存のファイルに追記します"
+     puts "追記したいファイル名を入力してください"
+     memo_type3 = gets.chomp
+     puts "編集したい内容を記入してください"
+     puts "完成したらCtrl + Dを押します"
+     memo_type = STDIN.read
+     CSV.open("#{memo_type3}.csv","a") do | csv |
+     csv << ["#{memo_type}"]
     end
     else
-    puts "エラー1か2を押してください"
+     puts "エラー1か2を押してください"
     end
